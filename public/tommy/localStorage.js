00aae273f0767b5cacd7fd08ba470564b41bf5d0
tommy.huang <cy94295@gmail.com>
2021-08-16 22:44:17 +0800

    add local Storage

--- public/tommy/localStorage.js
+++ public/tommy/localStorage.js
@@ -4,24 +4,24 @@ var bgcolorForm = document.getElementById('bgcolor')
 
 const initialization = () => {
     console.log('intialization... 檢查是否有初始值在 local Storage 中...')
-    if(sessionStorage.getItem('bgcolor')) {
-        htmlElement.style.backgroundColor = sessionStorage.getItem('bgcolor')
-        bgcolorForm.value = sessionStorage.getItem('bgcolor')
+    if(localStorage.getItem('bgcolor')) {
+        htmlElement.style.backgroundColor = localStorage.getItem('bgcolor')
+        bgcolorForm.value = localStorage.getItem('bgcolor')
     }
 }
 
 initialization()
 
 function populateState() {
-    console.log('populate state in sessionStorage')
-    sessionStorage.setItem('bgcolor', document.getElementById('bgcolor').value)
+    console.log('populate state in localStorage')
+    localStorage.setItem('bgcolor', document.getElementById('bgcolor').value)
     setStyles()
 }
 
 const setStyles = () => {
-    var bgcolorValue = sessionStorage.getItem('bgcolor')
+    var bgcolorValue = localStorage.getItem('bgcolor')
     htmlElement.style.backgroundColor = bgcolorValue
-    console.log('the bgcolorValue in sessionStorage is= ' + bgcolorValue)
+    console.log('the bgcolorValue in localStorage is= ' + bgcolorValue)
 }
 
 bgcolorForm.onchange = populateState;
