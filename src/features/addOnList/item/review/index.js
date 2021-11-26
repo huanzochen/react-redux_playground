ba4b4919f8088eb8dfb833719c0ae56b74c90d09
tzuchuan.huang <tzuchuan.huang@rakuten.com>
2021-11-26 22:02:29 +0800

    add review color

--- src/features/addOnList/item/review/index.js
+++ src/features/addOnList/item/review/index.js
@@ -2,14 +2,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar as faFullStar } from "@fortawesome/free-solid-svg-icons";
 import { faStar as faEmptyStar } from "@fortawesome/free-regular-svg-icons";
 
+import { StyledReview } from "./styleReview";
+
 const Review = ({ reviewStars, reviewCount }) => {
   console.log("reviewStars", reviewStars);
   console.log("reviewCount", reviewCount);
 
   return (
-    <div>
+    <StyledReview>
       <FontAwesomeIcon icon={faFullStar}></FontAwesomeIcon>
-    </div>
+    </StyledReview>
   );
 };
 
