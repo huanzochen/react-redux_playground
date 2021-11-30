6cde22ebbd84cb7e52fd589c371f6aae97f9bbd4
tzuchuan.huang <tzuchuan.huang@rakuten.com>
2021-11-30 21:29:57 +0800

    modify ApolloClient

--- src/features/apolloPlayground/addTodo/context/addTodoWrapper.js
+++ src/features/apolloPlayground/addTodo/context/addTodoWrapper.js
@@ -1,9 +1,13 @@
-import ApolloClient, { gql } from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { ApolloClient } from "apollo-client";
 import { ApolloProvider } from "@apollo/react-hooks";
 
 const client = new ApolloClient({
   // This instance related on
   uri: "https://plp0mopxq.sse.codesandbox.io/",
+  link: new HttpLink(),
+  cache: new InMemoryCache(),
 });
 
 const AddTodoWrapper = ({ children }) => {
