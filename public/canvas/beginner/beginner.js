74476e14dd0d8efc3ab6f246289ae3ddccefb3da
tzuchuan.huang <cy94295@gmail.com>
2025-02-15 21:34:20 +0800

    feat: boundary check

--- public/canvas/beginner/beginner.js
+++ public/canvas/beginner/beginner.js
@@ -1,11 +1,17 @@
-const canvas = document.querySelector('canvas')
+// debug
+console.log('window.innerWidth:', window.innerWidth, 'window.innerHeight:', window.innerHeight)
+console.log('document.documentElement.clientWidth:', document.documentElement.clientWidth,
+  '\ndocument.documentElement.clientHeight: ', document.documentElement.clientHeight)
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+const canvas = document.querySelector('canvas')
+const globalWidth = document.documentElement.clientWidth
+const globalHeight = document.documentElement.clientHeight
+canvas.width = globalWidth
+canvas.height = globalHeight
+
+console.log('canvas.width:', canvas.width, '\n canvas.height:', canvas.height)
 
 const c = canvas.getContext('2d')
-
-console.log(canvas)
 
 // Square
 
@@ -17,16 +23,29 @@ console.log(canvas)
 // c.fillRect(400, 100, 100, 100)
 // c.fillRect(300, 300, 100, 100)
 
-for (let i = 0; i < 10; i++) {
+const genStartInBoundary = (boundary, length) => {
+  let start
+  let end
+
+  const offset = 20
+  do {
+    start = Math.random() * boundary
+    end = start + length
+  } while ((end > boundary - offset) || (start < offset))
+
+  return start
+}
+
+for (let i = 0; i < 20; i++) {
   const redColor = Math.random() * 255
   const greenColor = Math.random() * 255
   const blueColor = Math.random() * 255
 
-  const xStart = Math.random() * window.innerHeight
-  const yStart = Math.random() * window.innerWidth
-
   const width = 100
   const height = 100
+
+  const xStart = genStartInBoundary(globalWidth, width)
+  const yStart = genStartInBoundary(globalHeight, height)
 
   c.beginPath()
   c.fillStyle = `rgba(${redColor},${greenColor},${blueColor})`
@@ -41,16 +60,37 @@ for (let i = 0; i < 10; i++) {
 // c.strokeStyle = '#fa34a3'
 // c.stroke()
 
-for (let i = 0; i < 10; i++) {
-  let xStart = Math.random() * window.innerWidth
-  let yStart = Math.random() * window.innerWidth
+const gen2DStartAndEndInBound = (boundaryX, boundaryY, length) => {
+  const offset = 20
+  let xStart, yStart, xEnd, yEnd
 
-  let xGoal = 300
-  let yGoal = 300
+  const checkOutOfBOund = () => {
+    return (
+      xStart < offset || xStart > boundaryX - offset ||
+      xEnd < offset || xEnd > boundaryX - offset ||
+      yStart < offset || yStart > boundaryY - offset ||
+      yEnd < offset || yEnd > boundaryY - offset
+    )
+  }
+
+  do {
+    xStart = Math.random() * boundaryX
+    yStart = Math.random() * boundaryY
+    const angleInDegrees = Math.random() * 360;
+    const angleInRadians = angleInDegrees * (Math.PI / 180);
+    xEnd = xStart + length * Math.cos(angleInRadians)
+    yEnd = yStart + length * Math.sin(angleInRadians)
+  } while (checkOutOfBOund())
+
+  return [xStart, yStart, xEnd, yEnd];
+}
+
+for (let i = 0; i < 20; i++) {
+  const [xStart, yStart, xEnd, yEnd] = gen2DStartAndEndInBound(globalWidth, globalHeight, 100)
 
   c.beginPath()
   c.moveTo(xStart, yStart)
-  c.lineTo(xGoal, yGoal)
+  c.lineTo(xEnd, yEnd)
   c.strokeStyle = '#fa34a3'
   c.stroke()
 }
@@ -61,13 +101,13 @@ for (let i = 0; i < 10; i++) {
 // c.strokeStyle = 'blue'
 // c.stroke()
 
-for (let i = 0; i < 30; i++) {
-  console.log('Math.random():', Math.random(), 'window.innerWidth:', window.innerWidth)
-
-  let x = Math.random() * window.innerWidth
-  let y = Math.random() * window.innerHeight
+for (let i = 0; i < 20; i++) {
+  const offset = 10
+  const radius = 30
+  let x = Math.random() * (globalWidth - 2 * (radius + offset)) + (radius + offset)
+  let y = Math.random() * (globalHeight - 2 * (radius + offset)) + (radius + offset)
   c.beginPath()
-  c.arc(x, y, 30, 0, Math.PI * 2, false)
+  c.arc(x, y, radius, 0, Math.PI * 2, false)
   c.strokeStyle = 'blue'
   c.stroke()
 }
